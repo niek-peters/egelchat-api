f55e3e3b3f73ba2c6a99a20ad91a3be5711f843b
MrValk <n.s.peters05@gmail.com>
2022-08-08 18:12:52 +0200

    Improved the file type checking security

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -10,6 +10,8 @@ import { User, UserDB, generateAuthToken } from "../models/user.js";
 import { db } from "../index.js";
 import { createBinaryUUID, fromBinaryUUID, toBinaryUUID } from "binary-uuid";
 
+import { fileTypeFromFile } from "file-type";
+
 const uploadDir = "../public/";
 
 const storage = multer.diskStorage({
@@ -24,17 +26,18 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage: storage,
-  fileFilter: (req, file, cb) => {
+  fileFilter: async (_req, file, cb) => {
     if (
-      file.mimetype == "image/png" ||
-      file.mimetype == "image/jpg" ||
-      file.mimetype == "image/jpeg"
+      !(
+        file.mimetype == "image/png" ||
+        file.mimetype == "image/jpg" ||
+        file.mimetype == "image/jpeg"
+      )
     ) {
-      cb(null, true);
-    } else {
-      cb(null, false);
-      return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+      return cb(new Error("Only .png, .jpg and .jpeg format allowed"));
     }
+
+    cb(null, true);
   },
 }).single("pf_pic");
 
@@ -44,63 +47,72 @@ router.put(
   "/",
   [auth, sameUser, upload],
   async (req: Request, res: Response) => {
-    // User's uuid
-    const uuid = res.locals.user.uuid;
+    try {
+      if (!req.file) return res.status(400).send("No file uploaded.");
 
-    const base64Uuid = toBinaryUUID(uuid).toString("base64url");
+      // User's uuid
+      const uuid = res.locals.user.uuid;
 
-    if (!req.file) return res.status(400).send("No file uploaded.");
+      const base64Uuid = toBinaryUUID(uuid).toString("base64url");
 
-    const base64ImgUuid = createBinaryUUID().buffer.toString("base64url");
+      const base64ImgUuid = createBinaryUUID().buffer.toString("base64url");
 
-    const filePath = `../public/${req.file.filename}`;
-    const newFilePath = `../public/pf_pic_${base64Uuid}_${base64ImgUuid}.webp`;
+      const filePath = `../public/${req.file.filename}`;
+      const newFilePath = `../public/pf_pic_${base64Uuid}_${base64ImgUuid}.webp`;
 
-    sharp.cache(false);
-    await sharp(filePath).resize(100, 100).toFile(newFilePath);
+      const type = await fileTypeFromFile(filePath);
 
-    glob(`../public/pf_pic_${base64Uuid}*`, (err, files) => {
-      if (err) {
-        console.log(err);
-      } else if (files.length !== 0) {
-        files.forEach((file) => {
-          if (!file.includes(base64ImgUuid))
-            unlink(file, (err) => {
-              if (err) {
-                console.log(err);
-              }
-            });
-        });
+      if (!type) {
+        unlink(filePath, (err) => (err ? console.error(err) : null));
+        return res.status(400).send("Could not determine file type");
       }
-    });
 
-    unlink(filePath, (err) => {
-      if (err) console.error(err);
-    });
+      if (!(type.mime == "image/png" || type.mime == "image/jpeg")) {
+        unlink(filePath, (err) => (err ? console.error(err) : null));
+        return res.status(400).send("Only .png, .jpg and .jpeg format allowed");
+      }
 
-    await db("Users")
-      .update({
-        pf_pic: `http://localhost:3000/pf_pic_${base64Uuid}_${base64ImgUuid}.webp`,
-      })
-      .where({ uuid: toBinaryUUID(uuid) });
+      sharp.cache(false);
+      await sharp(filePath).resize(100, 100).toFile(newFilePath);
 
-    const result: UserDB = (
-      await db("Users").where({
-        uuid: toBinaryUUID(uuid),
-      })
-    )[0];
+      glob(`../public/pf_pic_${base64Uuid}*`, (err, files) => {
+        if (err) {
+          console.log(err);
+        } else if (files.length !== 0) {
+          files.forEach((file) => {
+            if (!file.includes(base64ImgUuid))
+              unlink(file, (err) => (err ? console.error(err) : null));
+          });
+        }
+      });
 
-    const user: User = {
-      uuid: fromBinaryUUID(result.uuid),
-      name: result.name,
-      email: result.email,
-      pf_pic: result.pf_pic,
-    };
+      unlink(filePath, (err) => (err ? console.error(err) : null));
 
-    const token = generateAuthToken(user);
+      await db("Users")
+        .update({
+          pf_pic: `http://localhost:3000/pf_pic_${base64Uuid}_${base64ImgUuid}.webp`,
+        })
+        .where({ uuid: toBinaryUUID(uuid) });
 
-    res.header("Authorization", "Bearer " + token).send();
+      const result: UserDB = (
+        await db("Users").where({
+          uuid: toBinaryUUID(uuid),
+        })
+      )[0];
+
+      const user: User = {
+        uuid: fromBinaryUUID(result.uuid),
+        name: result.name,
+        email: result.email,
+        pf_pic: result.pf_pic,
+      };
+
+      const token = generateAuthToken(user);
+
+      res.header("Authorization", "Bearer " + token).send();
+    } catch (err) {
+      if (err instanceof Error) console.error(err.message);
+    }
   }
 );
-
 export default router;
